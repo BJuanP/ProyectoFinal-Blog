bbf076ea435ef9d6e3d67d14ab806d582766a5be
Lussad <yaelucas00@gmail.com>
2025-08-02 00:32:39 -0300

    modelo de comentario y agregado en admin.py

--- Blog/apps/comentarios/models.py
+++ Blog/apps/comentarios/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from articulos.models import Articulo
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Comentario(models.Model):
+    creado = models.DateTimeField(auto_now_add= True)
+    modificado = models.DateTimeField(auto_now=True)
+    texto = models.TextField(max_length=600)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE, null=True)
